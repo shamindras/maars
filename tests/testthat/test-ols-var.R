da3420b69fce4089ddffe044b0bd6d98c1a010c7
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2020-11-17 15:39:03 -0500

    FT: #8 Add broom to dependencies

--- tests/testthat/test-ols-var.R
+++ tests/testthat/test-ols-var.R
@@ -1,10 +1,12 @@
 test_that("lm and sandwhich variance - 4 dec.pl.", {
-    n = 2e4
-    X <- rnorm(n, 0, 1)
-    y <- 2 + X * 1 + rnorm(n, 0, 10)
-    lm_fit = lm(y ~ X)
-    sandwich_var = comp_sandwich_var(X, lm_fit$residuals)
-  expect_equal(round(broom::tidy(lm_fit)$std.error,3),
-               round(sqrt(diag(sandwich_var)),3))
+  n <- 2e4
+  X <- rnorm(n, 0, 1)
+  y <- 2 + X * 1 + rnorm(n, 0, 10)
+  lm_fit <- lm(y ~ X)
+  sandwich_var <- comp_sandwich_var(X, lm_fit$residuals)
+  expect_equal(
+    round(broom::tidy(lm_fit)$std.error, 3),
+    round(sqrt(diag(sandwich_var)), 3)
+  )
 })
 
