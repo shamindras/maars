d0ae6b75d2ae0949f33695e5e7b46f2929761a5a
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2020-11-18 23:42:37 +0100

    FT: #8 speed up sandwich estimator using QR decomposition and Matrix package; add test against sandwich estimator from sandwich package

--- R/ols-var.R
+++ R/ols-var.R
@@ -1,18 +1,15 @@
 #' Compute the White sandwich estimator for ordinary least squares (OLS) regression.
 #'
-#' @param X (matrix) : independent variables used in OLS fit
-#' @param residuals (numeric) : residuals from OLS fit
+#' @param lm_object (lm) : lm object
 #'
 #' @return (matrix) : White sandwich estimator of variance for OLS regression
 #' @export
 #'
 #' @examples
-comp_sandwich_var <- function(X, residuals) {
-  # TODO: need to check if X has intercept
-  X <- cbind(1, X)
-  J_inv <- solve(t(X) %*% X)
-  V <- t(X) %*% diag(residuals^2) %*% X
-  base::return(unname(J_inv %*% V %*% J_inv))
+comp_sandwich_qr_var <- function(lm_object){
+  qR <- qr.R(lm_object$qr)
+  J_inv <- chol2inv(qR)
+  X <- qr.X(lm_object$qr)
+  V <- t(X) %*% Matrix::Diagonal(x = lm_object$residuals^2) %*% X
+  return(unname(as.matrix(J_inv %*% V %*% J_inv)))
 }
-
-
--- tests/testthat/test-ols-var.R
+++ tests/testthat/test-ols-var.R
@@ -1,12 +1,25 @@
+set.seed(1)
 test_that("lm and sandwhich variance - 4 dec.pl.", {
-  n <- 2e4
-  X <- rnorm(n, 0, 1)
-  y <- 2 + X * 1 + rnorm(n, 0, 10)
-  lm_fit <- lm(y ~ X)
-  sandwich_var <- comp_sandwich_var(X, lm_fit$residuals)
+  n <- 1e5
+  X <- stats::rnorm(n, 0, 1)
+  y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
+  lm_fit <- stats::lm(y ~ X)
+  sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
   expect_equal(
-    round(broom::tidy(lm_fit)$std.error, 3),
-    round(sqrt(diag(sandwich_var)), 3)
+    round(broom::tidy(lm_fit)$std.error, 4),
+    round(sqrt(diag(sandwich_qr_var)), 4)
   )
 })
 
+test_that("sandwich variance from our estimator and estimator from Sandwich pkg - 4 dec.pl.", {
+  n <- 1e5
+  X <- stats::rnorm(n, 0, 1)
+  y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
+  lm_fit <- stats::lm(y ~ X)
+  sandwich_sandpkg_var <- sandwich::sandwich(lm_fit)
+  sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
+  expect_equal(
+    c(round(sandwich_sandpkg_var, 4)),
+    c(round(sandwich_qr_var, 4))
+  )
+})
