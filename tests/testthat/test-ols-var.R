8f777c56c3e07a428e996f0e00be538a380c35c3
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2020-11-20 10:47:49 +0100

    FT: #8 change seed and structure of test

--- tests/testthat/test-ols-var.R
+++ tests/testthat/test-ols-var.R
@@ -1,25 +1,23 @@
-set.seed(1)
-test_that("lm and sandwhich variance - 4 dec.pl.", {
-  n <- 1e5
-  X <- stats::rnorm(n, 0, 1)
-  y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
-  lm_fit <- stats::lm(y ~ X)
-  sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
+set.seed(1246426)
+
+n <- 1e5
+X <- stats::rnorm(n, 0, 1)
+y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
+lm_fit <- stats::lm(y ~ X)
+sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
+NUM_DEC_PL <- 4
+
+test_that("lm and sandwhich variance", {
   expect_equal(
-    round(broom::tidy(lm_fit)$std.error, 4),
-    round(sqrt(diag(sandwich_qr_var)), 4)
+    round(broom::tidy(lm_fit)$std.error, NUM_DEC_PL),
+    round(sqrt(diag(sandwich_qr_var)), NUM_DEC_PL)
   )
 })
 
-test_that("sandwich variance from our estimator and estimator from Sandwich pkg - 4 dec.pl.", {
-  n <- 1e5
-  X <- stats::rnorm(n, 0, 1)
-  y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
-  lm_fit <- stats::lm(y ~ X)
+test_that("sandwich variance from our estimator and estimator from Sandwich pkg", {
   sandwich_sandpkg_var <- sandwich::sandwich(lm_fit)
-  sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
   expect_equal(
-    c(round(sandwich_sandpkg_var, 4)),
-    c(round(sandwich_qr_var, 4))
+    c(round(sandwich_sandpkg_var, NUM_DEC_PL)),
+    c(round(sandwich_qr_var, NUM_DEC_PL))
   )
 })
