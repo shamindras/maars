708633de13bb0b02c9d9f8c421a69a0bc8f70fc5
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2020-12-01 16:22:08 -0500

    FT: #20 Add 5 types of multiplier bootstrap weights, update tests

--- R/ols-multiplier-boot-var.R
+++ R/ols-multiplier-boot-var.R
@@ -1,3 +1,89 @@
+#' Generate Different Types of Multiplier Bootstrap Weights
+#'
+#' A Helper function for \code{\link[maar]{comp_multiplier_bootstrap_var}}
+#' to generate different types of Multiplier Bootstrap
+#' Weights. This section is inspired by the \code{weighttype} option in the
+#' \code{Stata boottest} package.
+#'
+#' @param n The number of random Multiplier bootstrap weights to generate
+#' @param weights_type The type of Multiplier bootstrap weights to generate.
+#' Based on the \code{weighttype} option in the \code{Stata boottest} package,
+#' this can only take the following five prespecified values
+#' \code{"rademacher", "mammen", "webb", "std_gaussian", "gamma"}. A brief
+#' description of each type is noted as below. The \code{"rademacher"} weights
+#' are sampled from the two point Rademacher distribution which has takes values
+#' \eqn{\{1, 1\}}, with equal probability. The \code{"mammen"} weights
+#' are sampled from the two point Mammen distribution which has takes values
+#' \eqn{\{\phi, 1 - \phi\}}, with probabilities
+#' \eqn{\{\frac{\phi}{\sqrt{5}}, 1 - \frac{\phi}{\sqrt{5}}\}}, respectively. The
+#' \code{"webb"} weights are sampled from the six point Webb distribution
+#' which has takes values \eqn{\{\pm \sqrt{\frac{3}{2}}, \pm \sqrt{\frac{1}{2}}, \pm 1 \}},
+#' with equal probability. The \code{"std_gaussian"} weights are sampled from
+#' the standard Gaussian (normal) distribution with mean = \eqn{0},
+#' and variance = \eqn{1}. Finally, the \code{"gamma"} weights are sampled from
+#' the Gamma distribution with shape parameter = \eqn{4}, and scale parameter = \eqn{1}.
+#' For more details on these weight types see \insertCite{@see @roodman2019fastwildinferencestataboottest;textual}{maar} for more details.
+#'
+#' @return A numeric vector of n (sampled with replacement) random Multiplier
+#' bootstrap weights based on the specified Multiplier weights type
+#'
+#' @export
+#'
+#' @importFrom Rdpack reprompt
+#'
+#' @references \insertAllCited
+#'
+#' @examples
+#' \dontrun{
+#' # Number of Multiplier weights to generate
+#' n <- 1000
+#'
+#' # Generate the different type of Multiplier Weights
+#' rademacher_w <- gen_multiplier_bootstrap_weights(n = n,
+#'                                                  weights_type = "rademacher")
+#' mammen_w <- gen_multiplier_bootstrap_weights(n = n, weights_type = "mammen")
+#' webb_w <- gen_multiplier_bootstrap_weights(n = n, weights_type = "webb")
+#' std_gaussian_w <- gen_multiplier_bootstrap_weights(n = n,
+#'                                                    weights_type = "std_gaussian")
+#' gamma_w <- gen_multiplier_bootstrap_weights(n = n, weights_type = "gamma")
+#' }
+gen_multiplier_bootstrap_weights <- function(n, weights_type) {
+  assertthat::assert_that(n == as.integer(n),
+                          msg = glue::glue("n must be an integer"))
+  assertthat::assert_that(n > 0,
+                          msg = glue::glue("n must be positive"))
+  assertthat::assert_that(length(weights_type) == 1,
+                          msg = glue::glue("weights_type must only be a single value"))
+  assertthat::assert_that(weights_type %in% c("rademacher", "mammen",
+                                              "webb", "std_gaussian", "gamma"),
+                          msg = glue::glue("weights_type must only be one of the following types c('rademacher', 'mammen', 'webb', 'std_gaussian', 'gamma')"))
+
+  if (weights_type == "rademacher") {
+    out <- sample(
+      x = c(-1, 1), n = n, replace = TRUE,
+      prob = rep(x = 1 / 2, times = 2)
+    )
+  } else if (weights_type == "mammen") {
+    phi <- (1 + sqrt(5)) / 2 # Golden ratio
+    out <- sample(
+      x = c(1 - phi, phi), n = n, replace = TRUE,
+      prob = c(phi / sqrt(5), 1 - phi / sqrt(5))
+    )
+  } else if (weights_type == "webb") {
+    webb_neg_supp <- c(-sqrt(3 / 2), -1, -sqrt(1 / 2)) # Negative Webb weights
+    out <- sample(
+      x = c(webb_neg_supp, -webb_neg_supp),
+      n = n, replace = TRUE,
+      prob = rep(x = 1 / 6, times = 6)
+    )
+  } else if (weights_type == "std_gaussian") {
+    out <- stats::rnorm(n = n, mean = 0, sd = 1)
+  } else if (weights_type == "gamma") {
+    out <- stats::rgamma(n = n, shape = 4, scale = 1 / 2)
+  }
+  return(out)
+}
+
 #' Calculate a single replication of the multiplier bootstrap for an OLS fitted dataset
 #'
 #' Calculate a single replication of the multiplier bootstrap for an OLS fitted dataset based on an \code{lm} fitted object in \code{R}.
@@ -81,7 +167,7 @@ comp_multiplier_single_bootstrap_var <- function(n, J_inv_X_res, e) {
 #' set.seed(162632)
 #' comp_multiplier_bootstrap_var(lm_fit, B = 15)
 #' }
-comp_multiplier_bootstrap_var <- function(mod_fit, B = 100) {
+comp_multiplier_bootstrap_var <- function(mod_fit, B = 100, weights_type) {
   assertthat::assert_that(all("lm" == class(mod_fit)),
                           msg = glue::glue("mod_fit must only be of class lm"))
   assertthat::assert_that(B == as.integer(B),
@@ -99,7 +185,11 @@ comp_multiplier_bootstrap_var <- function(mod_fit, B = 100) {
                   do.call(rbind, .)
 
   # Multiplier weights (mean 0, variance = 1)
-  e <- matrix(data = rnorm(n = B * n, mean = 0, sd = 1), nrow = B, ncol = n)
+  e <- matrix(data =
+                maar::gen_multiplier_bootstrap_weights(n = B * n,
+                                                       weights_type = weights_type),
+              nrow = B,
+              ncol = n)
 
   # Multiplier Bootstrap replications, B times
   boot_out <- 1:B %>%
--- tests/testthat/test-ols-multiplier-boot-var.R
+++ tests/testthat/test-ols-multiplier-boot-var.R
@@ -8,12 +8,15 @@ X <- stats::rnorm(n, 0, 1)
 y <- 2 + X * 1 + stats::rnorm(n, 0, 1)
 lm_fit <- stats::lm(y ~ X)
 MAX_DIFF_high_precision <- 1e-10
+WEIGHTS_TYPE <- "std_gaussian"
 
 test_that("Check matrix and purrr multiplier bootstrap implmentations", {
     set.seed(5444243)
-    mult_boot_1 <- comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = 3)
+    mult_boot_1 <- comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = 3,
+                                                 weights_type = WEIGHTS_TYPE)
     set.seed(5444243)
-    mult_boot_2 <- comp_multiplier_bootstrap_purrr_var(mod_fit = lm_fit, B = 3)
+    mult_boot_2 <- comp_multiplier_bootstrap_purrr_var(mod_fit = lm_fit, B = 3,
+                                                       weights_type = WEIGHTS_TYPE)
     testthat::expect_equal(object = mult_boot_1,
                            expected = mult_boot_2,
                            tolerance = MAX_DIFF_high_precision)
@@ -21,9 +24,11 @@ test_that("Check matrix and purrr multiplier bootstrap implmentations", {
 
 test_that("Check matrix and purrr multiplier bootstrap implmentations, 30 replications", {
     set.seed(1626323)
-    mult_boot_1 <- comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = 30)
+    mult_boot_1 <- comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = 30,
+                                                 weights_type = WEIGHTS_TYPE)
     set.seed(1626323)
-    mult_boot_2 <- comp_multiplier_bootstrap_purrr_var(mod_fit = lm_fit, B = 30)
+    mult_boot_2 <- comp_multiplier_bootstrap_purrr_var(mod_fit = lm_fit, B = 30,
+                                                       weights_type = WEIGHTS_TYPE)
     testthat::expect_equal(object = mult_boot_1,
                            expected = mult_boot_2,
                            tolerance = MAX_DIFF_high_precision)
@@ -31,9 +36,12 @@ test_that("Check matrix and purrr multiplier bootstrap implmentations, 30 replic
 
 testthat::test_that("Check assertions are handled correctly", {
     # Check that B is an integer
-    expect_error(comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = 3.5))
+    expect_error(comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = 3.5,
+                                               weights_type = WEIGHTS_TYPE))
     # Check that B is positive
-    expect_error(comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = -1))
+    expect_error(comp_multiplier_bootstrap_var(mod_fit = lm_fit, B = -1,
+                                               weights_type = WEIGHTS_TYPE))
     # Check that lm_fit is an object of class "lm"
-    expect_error(comp_multiplier_bootstrap_var(mod_fit = mtcars, B = -1))
+    expect_error(comp_multiplier_bootstrap_var(mod_fit = mtcars, B = -1,
+                                               weights_type = WEIGHTS_TYPE))
 })
