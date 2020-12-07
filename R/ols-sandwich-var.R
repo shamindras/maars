d868eb75cb1c2efa74fe2dc1f53d39ea02b7ad09
Riccardo Fogliato <rfogliat@andrew.cmu.edu>
2020-12-07 01:24:39 +0100

    FT: #20 update sandwich standard errors and vignette

--- R/ols-sandwich-var.R
+++ R/ols-sandwich-var.R
@@ -7,7 +7,7 @@
 #'
 #' @param mod_fit An lm (OLS) object
 #'
-#' @return (matrix) White sandwich estimator of variance for OLS regression
+#' @return (tibble) White sandwich estimator of variance for OLS regression
 #'
 #' @export
 #'
@@ -21,13 +21,24 @@
 #' X <- stats::rnorm(n, 0, 1)
 #' y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
 #' lm_fit <- stats::lm(y ~ X)
-#' sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
+#' sandwich_qr_std_err <- comp_sandwich_qr_var(lm_fit)
 #' }
 comp_sandwich_qr_var <- function(mod_fit) {
   assertthat::assert_that(all("lm" == class(mod_fit)),
-                          msg = glue::glue("lm_object must only be of class lm"))
+                          msg = glue::glue("lm_object must only be of class lm")
+  )
   J_inv <- stats::summary.lm(mod_fit)$cov.unscaled
   X <- qr.X(mod_fit$qr)
   V <- t(X) %*% Matrix::Diagonal(x = stats::residuals(mod_fit)^2) %*% X
-  return(unname(as.matrix(J_inv %*% V %*% J_inv)))
+
+  out <- tibble::tibble(
+    term = names(mod_fit$coefficients),
+    estimate = mod_fit$coefficients,
+    std.error.sand = sqrt(diag(as.matrix(J_inv %*% V %*% J_inv)))
+  ) %>%
+    dplyr::mutate(
+      t.stat.sand = estimate/std.error.sand,
+      p.value.sand = 2*(1-sapply(abs(t.stat.sand), pnorm))
+    )
+  return(out)
 }
--- tests/testthat/test-ols-sandwich-var.R
+++ tests/testthat/test-ols-sandwich-var.R
@@ -4,20 +4,20 @@ n <- 1e5
 X <- stats::rnorm(n, 0, 1)
 y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
 lm_fit <- stats::lm(y ~ X)
-sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
-MAX_DIFF_low_precision <- 1e-5
+sandwich_qr_std_err <- comp_sandwich_qr_var(lm_fit)$std.error.sand
+MAX_DIFF_low_precision <- 1e-4
 MAX_DIFF_high_precision <- 1e-7
 
-test_that("lm and sandwhich variance", {
+test_that("lm and sandwhich standard errors", {
   expect_true(
-    max(abs(unname(diag(vcov(lm_fit))) - diag(sandwich_qr_var))) < MAX_DIFF_low_precision
+    max(abs(unname(sqrt(diag(vcov(lm_fit)))) - sandwich_qr_std_err)) < MAX_DIFF_low_precision
   )
 })
 
-test_that("sandwich variance from our estimator and estimator from Sandwich pkg", {
-  sandwich_sandpkg_var <- sandwich::sandwich(lm_fit)
+test_that("sandwich standard errors from our estimator and estimator from Sandwich pkg", {
+  sandwich_sandpkg_std_err <- sqrt(diag(sandwich::sandwich(lm_fit)))
   expect_true(
-    max(abs(c(sandwich_sandpkg_var) - c(sandwich_qr_var))) < MAX_DIFF_high_precision
+    max(abs(sandwich_sandpkg_std_err - sandwich_qr_std_err)) < MAX_DIFF_high_precision
   )
 })
 
@@ -26,15 +26,17 @@ test_that("sandwich variance from estimator via qr and lm", {
   X <- stats::rnorm(n, 0, 1)
   y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
   lm_fit <- stats::lm(y ~ X)
-  sandwich_qr_var <- comp_sandwich_qr_var(lm_fit)
+  sandwich_qr_std_err <- comp_sandwich_qr_var(lm_fit)$std.error.sand
   sandwich_lm_var_term <- coef(lm(diag(lm_fit$residuals) ~ 0 + qr.X(lm_fit$qr)))
   sandwich_lm_var <- sandwich_lm_var_term %*% t(sandwich_lm_var_term)
+  sandwich_lm_std_err <- sqrt(diag(sandwich_lm_var))
   expect_true(
-    max(abs(c(sandwich_lm_var) - c(sandwich_qr_var))) < MAX_DIFF_high_precision
+    max(abs(c(sandwich_lm_std_err) - c(sandwich_qr_std_err))) < MAX_DIFF_high_precision
   )
 })
 
 testthat::test_that("Check assertions are handled correctly", {
   # Check that lm_fit is an object of class "lm"
-  expect_error(comp_sandwich_qr_var(lm_object = mtcars))
+  expect_error(comp_sandwich_qr_var(mtcars))
 })
+
