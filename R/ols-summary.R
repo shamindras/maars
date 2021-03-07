16578f720be146c05abcad1384928590382d290c
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2021-03-07 00:01:03 -0500

    FT: #42 get the first version of get_var_tidy_summary working

---
+++ R/ols-summary.R
@@ -0,0 +1,277 @@
+#' Assertion Checks for individual \code{\link{get_summary}},
+#' \code{\link{get_assumptions}}, \code{\link{get_summary}} function inputs
+#'
+#' \code{check_fn_args_comp_var_boot_ind} is used to assess whether the arguments
+#' are correctly specified in \code{list} format and returns an error message if
+#' they do not match the correct specification
+#'
+#' @param mod_fit (maars_lm, lm) A fitted OLS \code{maars_lm, lm} class object
+#' @param sand (logical) : \code{TRUE} if sandwich estimator output is required,
+#'   \code{FALSE} to exclude this output from the request
+#' @param boot_emp (logical) : \code{TRUE} if empirical bootstrap standard error
+#'   output is required, \code{FALSE} to exclude this output from the request
+#' @param boot_res (logical) : \code{TRUE} if residual bootstrap standard error
+#'   output is required, \code{FALSE} to exclude this output from the request
+#' @param boot_mul (logical) : \code{TRUE} if multiplier bootstrap standard error
+#'   output is required, \code{FALSE} to exclude this output from the request
+#' @param well_specified (logical) : \code{TRUE} if lm standard errors
+#'   (well specified) output is required, \code{FALSE} to exclude this output
+#'   from the request
+#'
+#' @return : A character vector of  \code{TRUE} if assertions pass, or an
+#'   error if there is an assertion failure.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(1243434)
+#'
+#' # generate data
+#' n <- 1e3
+#' X_1 <- stats::rnorm(n, 0, 1)
+#' X_2 <- stats::rnorm(n, 10, 20)
+#' eps <- stats::rnorm(n, 0, 1)
+#'
+#' # OLS data and model
+#' y <- 2 + X_1 * 1 + X_2 * 5 + eps
+#' lm_fit <- stats::lm(y ~ X_1 + X_2)
+#'
+#' # Create a maars_lm model with empirical bootstrap, sandwich, and
+#' # well-specified variance estimates
+#' mss_var1 <- mss_var(mod_fit = lm_fit, boot_emp = list(B = 20, m = 200),
+#'                     boot_res = NULL,
+#'                     boot_mul = NULL)
+#'
+#' # Let the user request only the empirical bootstrap variance estimates
+#' # This function should return "boot_emp" since the user only requested it
+#' # and it is available in the fitted maars_lm model
+#' check_fn_args_summary(mod_fit = mss_var1, sand = FALSE,
+#'                       boot_emp = TRUE, boot_res = FALSE, boot_mul = FALSE,
+#'                       well_specified = FALSE)
+#' }
+check_fn_args_summary <- function(mod_fit, sand,
+                                  boot_emp, boot_res, boot_mul,
+                                  well_specified) {
+
+    # Assertion checking for mod_fit is of class "maars_lm", "lm"
+    assertthat::assert_that(all(c("maars_lm", "lm") == class(mod_fit)),
+                            msg = glue::glue("mod_fit must only be of class: ['maars_lm', 'lm']")
+    )
+
+    # Input variance parameter names
+    var_param_nms <- c("sand", "boot_emp", "boot_res", "boot_mul", "well_specified")
+    # Get the combined variance related parameter values
+    var_param_vals <- purrr::map(.x = var_param_nms, ~get(x = .x))
+    # Rename the list values to correspond to the input names
+    names(var_param_vals) <- var_param_nms
+    # var_param_vals <- c(sand, boot_emp, boot_res, boot_mul, well_specified)
+
+    # Check all input parameters, other than mod_fit are of class logical
+    assertthat::assert_that(
+        all(purrr::map_lgl(.x = var_param_vals, ~is.logical(.x))),
+        msg = glue::glue(
+            "All input variance parameters: [sand, boot_emp, boot_res, boot_mul, well_specified]",
+            "must be of class logical",
+            .sep = " "
+        ))
+
+    # Now that we have assertion checked all variance input parameters are of
+    # class logical, let's convert them into a logical vector (not list) to
+    # make future assertion checking easier
+    var_param_vals_lgl <- var_param_vals %>%
+        purrr::map_lgl(.x = ., ~ .x) %>%
+        base::unname(obj = .)
+
+    # If all parameters are FALSE, return a warning, and the table containing
+    # only the sandwich variance
+    if(!any(var_param_vals_lgl)){
+        warning(glue::glue("You have passed in FALSE for all input variance parameters:",
+                           "\n[sand, boot_emp, boot_res, boot_mul, well_specified].",
+                           "\n\nReturning the default sandwich variance estimator...\n",
+                           .sep = " "))
+        comm_nms <- c("var_sand")
+    } else{
+        # Filter to only the selected variance params i.e. those with TRUE values
+        var_param_nms_filt <- var_param_nms[var_param_vals_lgl]
+
+        # Get the comp_var output from the fitted maars_lm object
+        out_comp_var <- mod_fit$var
+        # out_comp_var_nms <- names(out_comp_var)
+        # Get the abbreviated names i.e. remove the "var_" prefix to align with summary
+        # inputs
+        # out_comp_var_nms_abb <- names(out_comp_var) %>%
+        #     stringr::str_replace_all(string = ., pattern = "var_", "")
+
+        # Filter out the non-NULL variance values
+        out_comp_var_filt <- out_comp_var %>% purrr::compact(.x = .)
+        # Get the abbreviated names i.e. with the "_var" prefix removed
+        out_comp_var_filt_nms_abb <- names(out_comp_var_filt) %>%
+            stringr::str_replace_all(string = ., pattern = "var_", "")
+
+        # Get the common variance estimates from the the model and the user
+        # requested variance estimates
+        # Here intersect first uses the model variance estimate names to ensure
+        # that the ordering of variance estimates is preserved from comp_var.
+        # This will ensure the correct default ordering in the consolidated
+        # variance summary terms in other functions that use this assertion
+        # check
+        comm_nms_abb <- intersect(out_comp_var_filt_nms_abb, var_param_nms_filt)
+
+        # Check that the user requested cterms are
+        assertthat::assert_that(
+            length(comm_nms_abb) == length(var_param_nms_filt),
+            msg = glue::glue("You have requested variance summary for",
+                             "[{glue::glue_collapse(var_param_nms_filt, sep = ' ')}]",
+                             "\nHowever your model only has the following variance computations:",
+                             "[{glue::glue_collapse(out_comp_var_filt_nms_abb, sep = ' ')}]",
+                             .sep = " "
+            )
+        )
+
+        # Add the "var_" prefix to the requested variance terms, so that we
+        # match the names in the variance estimates from the model i.e. from the
+        # comp_var output
+        comm_nms <- comm_nms_abb %>% stringr::str_c("var_", ., sep = "")
+    }
+
+    return(comm_nms)
+}
+
+
+#' An individual variance table summary function that adds the variance type
+#' suffix to the table columns
+#'
+#' @param comp_var_dat TODO Add
+#'
+#' @return TODO Add
+#'
+#' @keywords internal
+get_var_tidy_summary_ind <- function(comp_var_dat){
+    # TODO: Add assertion error that comp_var_dat is a list and is non_null
+    # TODO: Add assertion Check that the comp_var_dat has the correct names
+
+    # browser()
+
+    # DEFINE common column names - these stay the same across all
+    # reported error types. This is a global variable
+    COMMON_ERR_STAT_COLNAMES <- c("term", "estimate")
+
+    # Get the abbreviated variance type name
+    var_type_name_abb <- purrr::pluck(.x = comp_var_dat, "var_type_abb")
+
+    # Get the abbreviated variance summary table
+    var_dat_var_summ <- comp_var_dat %>% purrr::pluck("var_summary")
+
+    # Get all names except for term, estimate
+    err_comm_stat_names <- var_dat_var_summ %>%
+        base::colnames(x = .)
+
+    # Get the column names that should have the var type suffix added
+    err_stat_names_mod <- setdiff(err_comm_stat_names,
+                                  COMMON_ERR_STAT_COLNAMES)
+
+    # Create the modified column names with the var type suffix added
+    err_stat_names_mod <- err_stat_names_mod %>%
+        stringr::str_c(var_type_name_abb, sep = ".")
+    err_fin_stat_names <- c(COMMON_ERR_STAT_COLNAMES,
+                            err_stat_names_mod)
+
+    # Apply the modified column names with the var type suffix added on
+    # the standard error dataset
+    var_dat_var_summ <- var_dat_var_summ %>%
+        dplyr::rename_with(~ err_fin_stat_names, dplyr::everything())
+
+    base::return(var_dat_var_summ)
+}
+
+#' Get the tidy variance summary from a fitted OLS \code{maars_lm, lm}
+#' class object
+#'
+#' Get the tidy variance summary from a fitted OLS \code{maars_lm, lm}
+#' class object
+#'
+#' @param mod_fit (maars_lm, lm) A fitted OLS \code{maars_lm, lm} class object
+#' @param sand (logical) : \code{TRUE} if sandwich estimator output is required,
+#'   \code{FALSE} to exclude this output from the request
+#' @param boot_emp (logical) : \code{TRUE} if empirical bootstrap standard error
+#'   output is required, \code{FALSE} to exclude this output from the request
+#' @param boot_res (logical) : \code{TRUE} if residual bootstrap standard error
+#'   output is required, \code{FALSE} to exclude this output from the request
+#' @param boot_mul (logical) : \code{TRUE} if multiplier bootstrap standard error
+#'   output is required, \code{FALSE} to exclude this output from the request
+#' @param well_specified (logical) : \code{TRUE} if lm standard errors
+#'   (well specified) output is required, \code{FALSE} to exclude this output
+#'   from the request
+#'
+#' @return (tibble) : Combined standard error summary from a fitted
+#' OLS \code{maars_lm, lm} class object
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(1243434)
+#'
+#' # generate data
+#' n <- 1e3
+#' X_1 <- stats::rnorm(n, 0, 1)
+#' X_2 <- stats::rnorm(n, 10, 20)
+#' eps <- stats::rnorm(n, 0, 1)
+#'
+#' # OLS data and model
+#' y <- 2 + X_1 * 1 + X_2 * 5 + eps
+#' lm_fit <- stats::lm(y ~ X_1 + X_2)
+#'
+#' # DEFINE common column names - these stay the same across all
+#' # reported error types
+#' COMMON_ERR_STAT_COLNAMES <- c("term", "estimate")
+#'
+#' # Empirical Bootstrap check
+#' set.seed(454354534)
+#' mss_var1 <- mss_var(mod_fit = lm_fit, boot_emp = list(B = 20, m = 200),
+#'                     boot_res = list(B = 30),
+#'                     boot_mul = NULL)
+#'
+#' # This returns everything but boot_mul, since we didn't run it in the original
+#' # original maars_lm model
+#' get_var_tidy_summary(mod_fit = mss_var1, sand = TRUE,
+#'                      boot_emp = TRUE, boot_res = TRUE, boot_mul = FALSE,
+#'                      well_specified = TRUE)
+#' }
+get_var_tidy_summary <- function(mod_fit, sand = TRUE,
+                                 boot_emp = FALSE, boot_res = FALSE, boot_mul = FALSE,
+                                 well_specified = FALSE) {
+    # Get the variance types the user has requested. This performs assertion
+    # Checking, so if there is no error it will return the required names,
+    # else it will throw an error
+    req_var_nms <- check_fn_args_summary(mod_fit = mod_fit, sand = sand,
+                                         boot_emp = boot_emp, boot_res = boot_res,
+                                         boot_mul = boot_mul,
+                                         well_specified = well_specified)
+
+    # Get the comp_var output from the fitted maars_lm object
+    out_comp_var <- mod_fit$var
+
+    # Filter the comp_var output from the fitted maars_lm object for the
+    # requested variance types from the user
+    # out_comp_var_filt <- out_comp_var %>% purrr::pluck(req_var_nms)
+    out_comp_var_filt <- req_var_nms %>% purrr::map(.x = ., ~purrr::pluck(out_comp_var, .x))
+
+    out_comp_var_summ_filt <- out_comp_var_filt %>%
+                                purrr::map(.x = ., .f = purrr::pluck("var_summary"))
+
+
+    # Now for each individual variance type get the modified summary table
+    # with the abbreviated variance type added as a suffix
+    # e.g. for sandwich estimator, the 'p.value' column becomes 'p.value.sand'
+    out_comp_var_filt_mod <- out_comp_var_filt %>%
+        purrr::map(.x = ., .f = ~get_var_tidy_summary_ind(comp_var_dat = .x))
+
+    # Now return the left_join output of all tables
+    out <- out_comp_var_filt_mod %>%
+        purrr::reduce(.x = ., dplyr::left_join, by = c("term", "estimate"))
+
+    base::return(out)
+
+}
--- R/scripts_and_filters/experiments/experiment-get-summary.R
+++ R/scripts_and_filters/experiments/experiment-get-summary.R
@@ -1,93 +1,3 @@
-# Internal function
-fetch_tidy_summary_assumptions <- function(mod_fit, sand = TRUE,
-                        boot_emp = FALSE, boot_res = FALSE, boot_mul = FALSE,
-                        well_specified = FALSE) {
-
-  # Assertion checking for mod_fit is of class "maars_lm", "lm"
-  assertthat::assert_that(all(c("maars_lm", "lm") == class(mod_fit)),
-                          msg = glue::glue("mod_fit must only be of class: ['maars_lm', 'lm']")
-  )
-
-  # Input variance parameter names
-  var_param_nms <- c("sand", "boot_emp", "boot_res", "boot_mul", "well_specified")
-  # Get the combined variance related parameter values
-  var_param_vals <- purrr::map(.x = var_param_nms, ~get(x = .x))
-  # Rename the list values to correspond to the input names
-  names(var_param_vals) <- var_param_nms
-  # var_param_vals <- c(sand, boot_emp, boot_res, boot_mul, well_specified)
-
-    # Check all input parameters, other than mod_fit are of class logical
-  assertthat::assert_that(
-    all(purrr::map_lgl(.x = var_param_vals, ~is.logical(.x))),
-    msg = glue::glue(
-      "All input variance parameters: [sand, boot_emp, boot_res, boot_mul, well_specified]",
-      "must be of class logical",
-      .sep = " "
-    ))
-
-  # browser()
-
-  # Now that we have assertion checked all variance input parameters are of
-  # class logical, let's convert them into a logical vector (not list) to
-  # make future assertion checking easier
-  var_param_vals_lgl <- var_param_vals %>% purrr::map_lgl(.x = ., ~ .x)
-
-  # Get the comp_var output from the fitted maars_lm object
-  out_comp_var <- mod_fit$var
-  out_comp_var_nms <- names(out_comp_var)
-  # Get the abbreviated names i.e. remove the "var_" prefix to align with summary
-  # inputs
-  out_comp_var_nms_abb <- out_comp_var_nms %>%
-    stringr::str_replace_all(string = ., pattern = "var_", "")
-
-    # If all parameters are FALSE, return a warning, and the table containing
-    # only the sandwich variance
-    if(!any(var_param_vals_lgl)){
-        warning(glue::glue("You have passed in FALSE for all input variance parameters:",
-                "\n[sand, boot_emp, boot_res, boot_mul, well_specified].",
-                "\n\nReturning the default sandwich variance estimator...\n",
-                .sep = " "))
-      # Process the comp_var list and get the non-NULL valued variance lists
-      #
-      summary_out <- out_comp_var %>% purrr::pluck("var_sand") %>% purrr::map(.x = ., ~magic_summary(.x))
-      assumptions_out <- out_comp_var %>% purrr::pluck("var_sand") %>% purrr::map(.x = ., ~magic_assumptions(.x))
-      confint_out <- out_comp_var %>% purrr::pluck("var_sand") %>% purrr::map(.x = ., ~magic_assumptions(.x))
-      out <- list(summary_out = summary_out,
-                  assumptions_out = assumptions_out,
-                  confint_out = confint_out)
-      # Assertion check that we are not missing
-
-      # and get the column modified
-    }
-
-    # If all parameters are true, we should also return the standard lm
-  if(all(var_param_vals_lgl)){
-    # Assertion handling to be added, if they pass in all TRUE but maarslm doesn't have all the errors e.g. res = NULL
-    # We exit gracefully here with an assertion error
-    # lm %>% broom::tidy() %>% left_join()
-    # out_comp_var contains all of the things we are asking for
-    # Process the comp_var list and get the non-NULL valued variance lists
-    summary_out <- out_comp_var %>% purrr::map(.x = ., ~magic_summary(.x))
-    assumptions_out <- out_comp_var %>% purrr::map(.x = ., ~magic_assumptions(.x))
-    confint_out <- out_comp_var %>% purrr::map(.x = ., ~magic_assumptions(.x))
-    out <- list(summary_out = summary_out,
-                assumptions_out = assumptions_out,
-                confint_out = confint_out)
-  }
-
-
-  # Assertion check that the
-  # out_comp_var contains all of the things we are asking for
-  # Process the comp_var list and get the non-NULL valued variance lists
-  summary_out <- out_comp_var %>% purrr::map(.x = ., ~magic_summary(.x))
-  assumptions_out <- out_comp_var %>% purrr::map(.x = ., ~magic_assumptions(.x))
-  confint_out <- out_comp_var %>% purrr::map(.x = ., ~magic_assumptions(.x))
-  out <- list(summary_out = summary_out,
-              assumptions_out = assumptions_out,
-              confint_out = confint_out)
-
-}
-
 # Comp var examples ----
 devtools::load_all()
 set.seed(1243434)
@@ -108,46 +18,45 @@ COMMON_ERR_STAT_COLNAMES <- c("term", "estimate")
 
 # Empirical Bootstrap check
 set.seed(454354534)
-mss_var1 <- mss_var(mod_fit = lm_fit, boot_emp = list(B = 100, m = 200),
-                    boot_res = list(B = 100),
-                    boot_mul = list(B = 10, weights_type = 'rademacher'))
-# names(mss_var1)
-# class(mss_var1)
+mss_var1 <- mss_var(mod_fit = lm_fit, boot_emp = list(B = 20, m = 200),
+                    boot_res = list(B = 30),
+                    boot_mul = NULL)
 
-out_test_summ1 <- get_tidy_summary(mod_fit = mss_var1, sand = TRUE,
-                                   boot_emp = FALSE, boot_res = FALSE,
-                                   boot_mul = FALSE,
-                                   well_specified = FALSE)
+# This returns everything but boot_mul, since we didn't run it in the original
+# original maars_lm model
+get_var_tidy_summary(mod_fit = mss_var1, sand = TRUE,
+                     boot_emp = TRUE, boot_res = TRUE, boot_mul = FALSE,
+                     well_specified = TRUE)
 
+# This returns an error since we require the tidy summary with boot_mul, but
+# we have not run it in the original maars_lm model
+get_var_tidy_summary(mod_fit = mss_var1, sand = TRUE,
+                     boot_emp = TRUE, boot_res = TRUE, boot_mul = TRUE,
+                     well_specified = TRUE)
 
-# Create maars_lm object
-la_fit <- la_housing_data %>%
-  lm(y ~ x + z) %>%
-  maars::mss_var(mod_fit = .,
-                 sand = TRUE,
-                 boot_emp = list(B = 100, m = 10),
-                 boot_mul = list(B = 10, weights_type = 'rademacher'),
-                 boot_res = NULL)
+# We are passing in all FALSE values, should return the sandwich estimator
+# with a warning message
+get_var_tidy_summary(mod_fit = mss_var1, sand = FALSE,
+                     boot_emp = FALSE, boot_res = FALSE, boot_mul = FALSE,
+                     well_specified = FALSE)
 
-
-# If they want the tidy summary
-la_fit %>%
-  maars::get_summary(maars_lm_fit, emp = ...) %>%
-  dplyr::mutate(add ratio_columns) %>%
-  dplyr::relocate(do operations) %>%
-  knitr::kable(format the table here)
-
-# If they want assumptions only
-la_fit %>% maars::get_assumptions(maars_lm_fit, emp = ...)
-
-# If they want assumptions only
-la_fit %>% maars::get_confint(maars_lm_fit, emp = ..., quantile = c(0.05, 0.95))
-
-# Behind the scenes we call fetch for BOTH...
-
-
-get_summary # User facing, per our spec it outputs only the tibble of std errors - what maar_lm.summary
-get_assumptions # User facing, per our spec it outputs only the tibble of std errors - what maar_lm.summary
-get_confint # User facing
-
-fetch_summary_assumptions # Internal, returns a list of 2 things. A tidy tibble of all left_join std. errors, and character vector of assumptions
+# res_sand_warn <- check_fn_args_summary(mod_fit = mss_var1, sand = FALSE,
+#                                        boot_emp = FALSE, boot_res = FALSE, boot_mul = FALSE,
+#                                        well_specified = FALSE)
+# res_sand_warn
+#
+# res_sand_err <- check_fn_args_summary(mod_fit = mss_var1, sand = FALSE,
+#                                       boot_emp = FALSE, boot_res = TRUE, boot_mul = FALSE,
+#                                       well_specified = FALSE)
+# res_sand_err
+#
+# res_sand_val <- check_fn_args_summary(mod_fit = mss_var1, sand = TRUE,
+#                                       boot_emp = TRUE, boot_res = FALSE, boot_mul = FALSE,
+#                                       well_specified = TRUE)
+# res_sand_val
+#
+# testthat::expect_error(
+# res_sand_err <- check_fn_args_summary(mod_fit = mss_var1, sand = FALSE,
+#                                       boot_emp = FALSE, boot_res = TRUE, boot_mul = FALSE,
+#                                       well_specified = FALSE)
+# )
