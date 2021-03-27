45a890df62cbaf1371d27747741125dc8a999447
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2021-03-27 11:27:17 -0400

    FT: #55 Add maars package level doc, importFrom Rdpack, rlang

---
+++ R/maars-package.R
@@ -0,0 +1,13 @@
+#' @description
+#' To learn more about \code{maars}, start with the vignettes:
+#' `browseVignettes(package = "maars")`
+#' @keywords internal
+#' @importFrom Rdpack reprompt
+#' @importFrom rlang .data
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
