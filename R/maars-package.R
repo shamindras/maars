3b1ded3856e28e6fe1839bdc64a67ddfe51d1b42
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2021-03-30 09:49:38 -0400

    FT: #55 Fix R CMD check .'s that appear in pipelines

--- R/maars-package.R
+++ R/maars-package.R
@@ -6,6 +6,11 @@
 #' @importFrom rlang .data
 "_PACKAGE"
 
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+## recomended per comments here: https://stackoverflow.com/questions/66816638/no-visible-binding-for-global-variable?noredirect=1#comment118198647_66816638
+## previously used here: https://github.com/jennybc/googlesheets/blob/master/R/googlesheets.R#L15-L16
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
