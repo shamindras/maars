323202abae2ce1285a650ea8ddc90827ad734bc2
ricfog <31448214+ricfog@users.noreply.github.com>
2021-03-31 10:46:11 +0200

    FT: #59 change number of iterations in test for res. boot. that is failing

--- tests/testthat/test-ols-boot-residual.R
+++ tests/testthat/test-ols-boot-residual.R
@@ -16,14 +16,14 @@ test_that("test sample mean of coefficients estimated via bootstrap matches the 
 })
 
 test_that('variance is 0 when the model is well-specified and sample size is large',{
-    n <- 1e4
+    n <- 3e4
     X <- stats::rnorm(n, 0, 1)
     # OLS data and model
     y <- 2 + X * 1 + stats::rnorm(n, 0, 1)
     lm_fit <- stats::lm(y ~ X)
     boot_out <- comp_boot_res(lm_fit, B = 1e3)$boot_out %>% tidyr::unnest(cols = boot_out)
     expect_equal(boot_out %>% dplyr::group_by(term) %>%
-                     dplyr::summarize(sd_estimate = sd(estimate)) %>%
+                     dplyr::summarize(sd_estimate = var(estimate)) %>%
                    dplyr::pull(sd_estimate) %>% unname(),
                  c(0,0), tol = 1e-2)
 })
