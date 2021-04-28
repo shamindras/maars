ca15e23b493df5742400e4ee6b023dd45d3cf8df
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2021-04-28 16:04:53 -0400

    FT: #60 Add m = n unit tests for subsampling

--- tests/testthat/test-boot-empirical.R
+++ tests/testthat/test-boot-empirical.R
@@ -1,5 +1,8 @@
 set.seed(1243434)
 
+# Define global variables
+MAX_DIFF_high_precision <- 1e-7
+
 # thanks to Arun K. for providing the following function
 gen_reg_data <- function(n,
                          gamma){ # gamma ~ "degree of misspecification"
@@ -16,6 +19,9 @@ n <- 1e3
 df <- gen_reg_data(n = n, gamma = 0)
 lm_fit <- stats::lm(Y ~ X, data = df)
 
+
+# Test Empirical Bootstrap i.e. sampling WITH replacement for each of the B
+# replications
 
 test_that("draw from dataset with one observation works returns that observation", {
   boot <- comp_boot_emp_samples(data = data.frame(y = 1, x = 2), B = 100, m = 10) %>% tidyr::unnest(cols = data)
@@ -111,9 +117,60 @@ test_that("test bootstrap confidence intervals via percentile method for differe
 })
 
 
+# Test Subsampling Bootstrap i.e. sampling WITHOUT replacement for each of the B
+# replications
 
+# Generate data from a well specified linear model
+n <- 1e3
+X <- stats::rnorm(n, 0, 1)
+y <- 2 + X * 1 + stats::rnorm(n, 0, 10)
+lm_fit <- stats::lm(y ~ X)
 
+test_that("Test case where m > n for subsampling on OLS", {
+  # m = n + 1 i.e. m > n
+  expect_error(comp_var(mod_fit = lm_fit,
+                        boot_emp = NULL,
+                        boot_sub = list(B = 1e3, m = n + 1),
+                        boot_res = NULL,
+                        boot_mul = NULL))
 
+  # Add valid empirical bootstrap, but failing subsampling with
+  # m = n + 1 i.e. m > n
+  expect_error(comp_var(mod_fit = lm_fit,
+                        boot_emp = list(B = 1e3, m = n + 1),
+                        boot_sub = list(B = 1e3, m = n + 1),
+                        boot_res = NULL,
+                        boot_mul = NULL))
+})
 
+test_that("Test m = n for subsampling and compare error to well specified OLS fit", {
+  lm_fit_summ <- broom::tidy(lm_fit)
+  mod_fit0 <- comp_var(mod_fit = lm_fit,
+                       boot_emp = NULL,
+                       boot_sub = list(B = 1e3, m = n),
+                       boot_res = NULL,
+                       boot_mul = NULL)
 
+  mod_fit0_summ <- mod_fit0 %>%
+    get_summary(mod_fit = ., boot_sub = TRUE) %>%
+    tidyr::pivot_wider(names_from = stat_type, values_from = stat_val)
+
+  # Check that the standard errors for subsampling in the case m = n are close
+  # to zero, for the well specified model
+  mod_fit0_serr <- mod_fit0_summ %>% dplyr::pull(std.error)
+  expect_equal(mod_fit0_serr[1], 0, tol = MAX_DIFF_high_precision)
+  expect_equal(mod_fit0_serr[2], 0, tol = MAX_DIFF_high_precision)
+
+  # Check that we have computed the correct estimate values
+  est_vals <- mod_fit0_summ %>%
+                dplyr::select(term, estimate) %>%
+                dplyr::rename(estimate_sub = estimate) %>%
+                dplyr::left_join(lm_fit_summ, by = "term") %>%
+                dplyr::select(term, estimate_sub, estimate) %>%
+    dplyr::rename(estimate_ols = estimate) %>%
+    dplyr::mutate(abs_estimate_diff = abs(estimate_sub - estimate_ols)) %>%
+    dplyr::pull(abs_estimate_diff)
+  expect_equal(est_vals[1], 0, tol = MAX_DIFF_high_precision)
+  expect_equal(est_vals[2], 0, tol = MAX_DIFF_high_precision)
+})
 
